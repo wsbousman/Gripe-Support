d543624419e53e67dc4fa9882a6e111143fabb28
Matthew Sievers <mattersievers@gmail.com>
2021-09-29 18:09:30 -0500

    completed basic route information. still need Auth and more eyes.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,29 +1,64 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post } = require('../models');
+const { Post, Category } = require('../models');
 
 router.get('/', (req,res) => {
     Post.findAll({
-
+    attributes: {
+        include: [
+            'id',
+            'category_id',
+            [sequelize.literal('SELECT COUNT(*) FROM post WHERE post.category_id = 1)'), 'support_posts'],
+            [sequelize.literal('SELECT COUNT(*) FROM post)'), 'all_posts']
+        ],   
+        exclude: ['user_id']
+    }
     })
     .then( dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('splash-page', {
+        res.render('splash', {
             posts
             //count var
         });
+    });    
 });
 
 router.get('/home', (req,res) => {
-
+    Post.findAll({
+        attributes: {
+            include: [
+            'id',
+            'content',
+            'flagged',
+            'category_id',
+            'created_at'
+            ],
+            exclude: ['user_id']
+        }         
+    })
+    .then( dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            posts
+            //count var
+        });
+    });
 });
 
 router.get('/login', (req,res) => {
-
+   if(req.session.loggedIn) {
+       res.redirect('/');
+       return;
+   }
+   res.render('login');
 });
 
 router.get('/signup', (req,res) => {
-
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
 });
 
 module.exports = router;
